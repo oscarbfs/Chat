4bd6751be356495af2f7b1b63552158a6006b702
oscarbfs <oscar.borgesfs@gmail.com>
2022-01-31 08:55:09 -0300

    Loading Page

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const <Widget>[
+          Text(
+            'Carregando...',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(height: 10),
+          LinearProgressIndicator(
+            color: Colors.white,
+          ),
+        ],
+      )),
+    );
+  }
+}
