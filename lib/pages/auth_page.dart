ee9a3215f63970a702fa5d1bb0e5c8308b58bf7e
oscarbfs <oscar.borgesfs@gmail.com>
2022-02-03 11:07:35 -0300

    Lógica Auth #1

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -12,11 +12,20 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   bool _isLoading = false;
 
-  void _handleSubmit(AuthFormData formData) {
-    setState(() => _isLoading = true);
-    print('AuthPage');
-    print(formData.email);
-    setState(() => _isLoading = false);
+  Future<void> _handleSubmit(AuthFormData formData) async {
+    try {
+      setState(() => _isLoading = true);
+
+      if (formData.isLogin) {
+        //Login
+      } else {
+        //Signup
+      }
+    } catch (error) {
+      //Tratar erro
+    } finally {
+      setState(() => _isLoading = false);
+    }
   }
 
   @override
