bf2b77d7bdcfbc16cdf06a096b294d88a7b0f831
oscarbfs <oscar.borgesfs@gmail.com>
2022-02-04 10:16:15 -0300

    Lógica Auth #3

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -4,8 +4,11 @@ import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
+  static Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+
   ChatUser? get currentUser {
-    return null;
+    return _currentUser;
   }
 
   Stream<ChatUser?> get userChanges {}
