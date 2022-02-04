8825e483eb883656c418f61536c7fbb88c2e2b2f
oscarbfs <oscar.borgesfs@gmail.com>
2022-02-04 11:01:10 -0300

    Lógica Auth #4

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
@@ -6,17 +8,43 @@ import 'package:chat/core/services/auth/auth_service.dart';
 class AuthMockService implements AuthService {
   static Map<String, ChatUser> _users = {};
   static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+  });
 
   ChatUser? get currentUser {
     return _currentUser;
   }
 
-  Stream<ChatUser?> get userChanges {}
+  Stream<ChatUser?> get userChanges {
+    return _userStream;
+  }
 
   Future<void> signup(
-      String name, String email, String password, File image) async {}
+      String name, String email, String password, File image) async {
+    final newUser = ChatUser(
+      id: Random().nextDouble().toString(),
+      name: name,
+      email: email,
+      imageUrl: image.path,
+    );
 
-  Future<void> login(String email, String password) async {}
+    _users.putIfAbsent(email, () => newUser);
+    _updateUser(newUser);
+  }
 
-  Future<void> logout() async {}
+  Future<void> login(String email, String password) async {
+    _updateUser(_users[email]);
+  }
+
+  Future<void> logout() async {
+    _updateUser(null);
+  }
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
 }
