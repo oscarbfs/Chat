bc4bdd7ae8e99fea58790c2817eb325dcd4ab5e6
oscarbfs <oscar.borgesfs@gmail.com>
2022-02-08 10:51:13 -0300

    Chat Page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat/components/messages.dart';
+import 'package:chat/components/new_message.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -7,17 +9,44 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Chat page'),
-            TextButton(
-              onPressed: () {
-                AuthService().logout();
-              },
-              child: Text('sair'),
+      appBar: AppBar(
+        title: const Text('Oscar Chat'),
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
             ),
+            items: [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Container(
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Colors.red,
+                      ),
+                      SizedBox(width: 10),
+                      Text('Sair')
+                    ],
+                  ),
+                ),
+              ),
+            ],
+            onChanged: (value) {
+              if (value == 'logout') {
+                AuthService().logout();
+              }
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: const [
+            Expanded(child: Messages()),
+            NewMessage(),
           ],
         ),
       ),
