01a6cf53db77d4dd43eb0f4b5abb9b5e925bc100
oscarbfs <oscar.borgesfs@gmail.com>
2022-02-11 09:17:26 -0300

    Chat Notification Service #02

--- lib/core/services/notification/push_notification_service.dart
+++ lib/core/services/notification/push_notification_service.dart
@@ -1,7 +1,7 @@
 import 'package:chat/core/models/chat_notification.dart';
 import 'package:flutter/material.dart';
 
-class PushNotificationService with ChangeNotifier {
+class ChatNotificationService with ChangeNotifier {
   List<ChatNotification> _items = [];
 
   List<ChatNotification> get items {
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (_) => PushNotificationService(),
+          create: (_) => ChatNotificationService(),
         )
       ],
       child: MaterialApp(
