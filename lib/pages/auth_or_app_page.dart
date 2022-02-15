3c7b743654dccc462c0406c3ad0735de51b48dcd
oscarbfs <oscar.borgesfs@gmail.com>
2022-02-15 09:56:46 -0300

    Iniciando Firebase APP

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -3,24 +3,37 @@ import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:chat/pages/chat_page.dart';
 import 'package:chat/pages/loading_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'auth_page.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({Key? key}) : super(key: key);
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: StreamBuilder<ChatUser?>(
-      stream: AuthService().userChanges,
+    return FutureBuilder(
+      future: init(context),
       builder: (ctx, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return LoadingPage();
         } else {
-          return snapshot.hasData ? ChatPage() : AuthPage();
+          return StreamBuilder<ChatUser?>(
+            stream: AuthService().userChanges,
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return LoadingPage();
+              } else {
+                return snapshot.hasData ? ChatPage() : AuthPage();
+              }
+            },
+          );
         }
       },
-    ));
+    );
   }
 }
