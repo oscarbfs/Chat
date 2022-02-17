1ea652f6c108deeb19075a107f63d551ed0a7227
oscarbfs <oscar.borgesfs@gmail.com>
2022-02-17 09:43:07 -0300

    Autenticação Com Firebase #01

--- lib/core/services/auth/auth_service.dart
+++ lib/core/services/auth/auth_service.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:chat/core/models/chat_user.dart';
-import 'package:chat/core/services/auth/auth_mock_service.dart';
+import 'package:chat/core/services/auth/auth_firebase_service.dart';
 
 abstract class AuthService {
   ChatUser? get currentUser;
@@ -15,7 +15,7 @@ abstract class AuthService {
   Future<void> logout();
 
   factory AuthService() {
-    return AuthMockService();
-    // return AuthFirebaseService();
+    // return AuthMockService();
+    return AuthFirebaseService();
   }
 }
