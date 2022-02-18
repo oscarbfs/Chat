c64f0c2702a57aa29b4deaaa165cdad1674abbf4
oscarbfs <oscar.borgesfs@gmail.com>
2022-02-18 09:58:40 -0300

    Salvando Mensagens do Firestore #01

---
+++ lib/core/services/chat/chat_firebase_service.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'package:chat/core/models/chat_message.dart';
+import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatFireBaseService implements ChatService {
+  Stream<List<ChatMessage>> messagesStream() {
+    return Stream<List<ChatMessage>>.empty();
+  }
+
+  Future<ChatMessage?> save(String text, ChatUser user) async {
+    final store = FirebaseFirestore.instance;
+
+    // ChatMessage => Map<String, dynamic>
+    store.collection('chat').add({
+      'text': text,
+      'createdAt': DateTime.now().toIso8601String(),
+      'userId': user.id,
+      'userName': user.name,
+      'userImageURL': user.imageUrl,
+    });
+    return null;
+  }
+}
