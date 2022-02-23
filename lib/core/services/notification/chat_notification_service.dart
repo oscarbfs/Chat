df57dd404600700d1dd317a9a8584f1cc4836de7
oscarbfs <oscar.borgesfs@gmail.com>
2022-02-23 10:12:36 -0300

    Push Notification #01

--- lib/core/services/notification/chat_notification_service.dart
+++ lib/core/services/notification/chat_notification_service.dart
@@ -1,4 +1,5 @@
 import 'package:chat/core/models/chat_notification.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class ChatNotificationService with ChangeNotifier {
@@ -20,5 +21,33 @@ class ChatNotificationService with ChangeNotifier {
   void remove(int i) {
     _items.removeAt(i);
     notifyListeners();
+  }
+
+  // Push Notification
+
+  Future<void> init() async {
+    await _configureForeground();
+  }
+
+  Future<bool> get _isAuthorized async {
+    final messaging = FirebaseMessaging.instance;
+    final settings = await messaging.requestPermission();
+    return settings.authorizationStatus == AuthorizationStatus.authorized;
+  }
+
+  Future<void> _configureForeground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessage.listen(
+        (msg) {
+          if (msg.notification == null) return;
+          add(
+            ChatNotification(
+              title: msg.notification!.title ?? 'Não informado!',
+              body: msg.notification!.body ?? 'Não informado!',
+            ),
+          );
+        },
+      );
+    }
   }
 }
--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,9 +1,11 @@
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/core/services/notification/chat_notification_service.dart';
 import 'package:chat/pages/chat_page.dart';
 import 'package:chat/pages/loading_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 import 'auth_page.dart';
 
@@ -12,6 +14,7 @@ class AuthOrAppPage extends StatelessWidget {
 
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp();
+    await Provider.of<ChatNotificationService>(context, listen: false).init();
   }
 
   @override
